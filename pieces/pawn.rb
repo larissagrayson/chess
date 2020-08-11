b3bd50f6b07901ec651fa36f5cd14a318f2d473f
Larissa Grayson <59101865+larissagrayson@users.noreply.github.com>
2020-08-11 11:54:11 -0400

    Modified Pawn class to allow 2 spaces on first move

--- pieces/pawn.rb
+++ pieces/pawn.rb
@@ -2,23 +2,57 @@
 
 require_relative 'chess_piece.rb'
 
+
 class Pawn < ChessPiece
+
+  attr_reader :first_move
 
   # Constants
   ROW = 0
+  BLACK = "BLACK"
+  WHITE = "WHITE"
 
   def initialize(color)
-    if color.upcase == "BLACK"
+    if color.upcase == BLACK
       unicode="\u265F"
     else
       unicode="\u2659"
     end
-    super(type="Pawn", color, unicode)
+
+    super(type="Pawn", color.upcase, unicode)
+    @first_move = true
   end
 
-  # Checks if the move is 1 space up
+  # Checks if the move is 1 or 2 spaces forward
   def valid_move?(origin, destination)
-    return (destination[ROW] - origin[ROW]) == -1
+    distance = destination[ROW] - origin[ROW]
+    if @color == BLACK
+      if (distance == 2 && @first_move)
+        @first_move = false
+        return true
+      elsif (distance == 1)
+        if @first_move
+          @first_move = false
+        end
+        return true
+      else
+        return false
+      end
+    elsif @color == WHITE
+      if (distance == -2 && @first_move)
+        @first_move = false
+        return true
+      elsif (distance == -1)
+        if @first_move
+          @first_move = false
+        end
+        return true
+      else
+        return false
+      end
+    else
+      return false
+    end
   end
 
  # Returns an array of spaces crossed in move
